dea64bbd0206c055994854e65041b2c0e94d800b
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2020-11-09 20:44:03 -0500

    Update theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#171922`,
         display: `minimal-ui`,
         icon: `content/assets/rice-ball.png`,
       },
